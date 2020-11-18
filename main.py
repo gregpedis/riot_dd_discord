80c717997794820407180c6cfb9f4be0def70db6
gregpedis <trafalgar1618@hotmail.com>
2020-11-18 22:18:53 +0200

    Initial commit bullshit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import discord
+import random
+
+TOKEN = "Nzc4NTk1ODYwODUzNTU1MjAx.X7UR5g.9WVP_JCN4aBvej1xJiUtZa_E8zw"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+client.run(TOKEN)
